35f55510f62c3073ed600ac3b40e7fe30da243dd
GuoJinMing@mbp <tdouguo@163.com>
2021-06-25 19:40:20 +0800

    uv

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
-cat package.json | jq 'del(.version)' | jq '. + { "version": "sdkver" }'>>tmp
-mv tmp package.json
-sed -i '' 's/sdkver/'"$1"'/'  package.json
+MODEL_PATH="Unity-Logs-Viewer"
+cat ${MODEL_PATH}/package.json | jq 'del(.version)' | jq '. + { "version": "sdkver" }'>>tmp
+mv tmp ${MODEL_PATH}/package.json
+sed -i '' 's/sdkver/'"$1"'/'  ${MODEL_PATH}/package.json
 git add -A
 git commit -m "uv"
 git pull
 git push
-git subtree split --prefix=. --branch upm
-git tag $1 upm
-git push origin upm --tags
+git subtree split --prefix=${MODEL_PATH} --branch upm$1
+git tag $1 upm$1
+git push origin upm$1 --tags
